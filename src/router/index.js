68db8557fd03790e1b2125ca0953262fe1bc217b
lcc <13440816989@qq.com>
2019-07-12 23:51:11 +0800

    init gshop

---
+++ src/router/index.js
@@ -0,0 +1,51 @@
+// 路由器对象模块
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Msite from '../pages/MSite/MSite'
+import Search from '../pages/Search/Search'
+import Order from '../pages/Order/Order'
+import Profile from '../pages/Profile/Profile'
+import Login from '../pages/Login/Login'
+// 声明使用插件
+Vue.use(VueRouter);
+export default new VueRouter({
+// 所有路由
+  routes: [
+    {
+      path: '/msite',
+      component: Msite,
+      meta:{
+        showFooter:true
+      }
+    },
+    {
+      path: '/search',
+      component: Search,
+      meta:{
+        showFooter:true
+      }
+    },
+    {
+      path: '/order',
+      component: Order,
+      meta:{
+        showFooter:true
+      }
+    },
+    {
+      path: '/profile',
+      component: Profile,
+      meta:{
+        showFooter:true
+      }
+    },
+    {
+      path: '/login',
+      component: Login
+    },
+    {
+      path: '/',
+      redirect: '/msite'
+    }
+  ]
+})
